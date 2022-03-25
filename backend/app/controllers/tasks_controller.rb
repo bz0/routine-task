66893acfda11039b82178967172429331f2a0e40
bz0 <bzz0217@yahoo.co.jp>
2022-03-25 10:22:46 +0900

    task 一覧・登録・更新メソッド追加

--- backend/app/controllers/tasks_controller.rb
+++ backend/app/controllers/tasks_controller.rb
@@ -1,2 +1,26 @@
 class TasksController < ApplicationController
+    def index
+        tasks = Task.all
+        render status: 200, json: { tasks: tasks }
+    end
+
+    def create
+        @task = Task.new(name: params[:post][:name]) # パラメータをそのまま引数に入れることに抵抗感がある、バリデーションを通してから入れたい
+
+        if (@task.valid?) {
+            task.save
+            status = 200
+            json = { task: task }
+        }else{
+            status = 400 //リクエスト不正
+            json = { error: "タスク名が未入力です" }
+        }
+
+        render status: status, json: json
+    end
+
+    def update
+        tasks = Task.all
+        render status: 200, json: { tasks: tasks }
+    end
 end
