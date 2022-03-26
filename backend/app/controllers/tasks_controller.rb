2f1ecf5b4e40b3d2271984f32a10b1112bdbd7b4
bz0 <bzz0217@yahoo.co.jp>
2022-03-26 11:55:30 +0900

    taskcontroller 異常系の場合の処理を追加

--- backend/app/controllers/tasks_controller.rb
+++ backend/app/controllers/tasks_controller.rb
@@ -1,15 +1,36 @@
 class TasksController < ApplicationController
     def index
-        tasks = Task.all
+        tasks = Task.all # todo:全件取得しているがページネーションにしたい
         render status: 200, json: { tasks: tasks }
     end
 
     def create
-        @task = Task.new(name: params[:name]) # paramsをそのまま入れたくない
-        @task.save
+        begin
+            params.require(:name)
+
+            task = Task.new
+            task.name = params[:name]
+            
+            unless task.save!
+                raise StandardError, "タスクの登録に失敗しました"
+            end
+            
+            http_code = 200
+            json = {status:"OK", data:{task:task}}
+        rescue ActionController::ParameterMissing => e
+            # 未入力の場合
+            http_code = 400
+            json = {status:"NG", message: "リクエストが不正です"}
+        rescue StandardError => e
+            p [:e, e]
+            # DB登録に失敗した場合
+            http_code = 500
+            status = "NG"
+            json = {status:"NG", message: e.message}
+        end
 
         # todo:パラメータが不正な場合にエラーを返せるようにしたい
-        render status: 200, json: { status: "OK" }
+        render status: http_code, json: json
     end
 
     def update
