17882b7ef3632ba3683648da47172771084dfa93
bz0 <bzz0217@yahoo.co.jp>
2022-03-28 16:07:05 +0900

    rake: ログ出力削除、メッセージ調整

--- backend/app/models/random_task_slack_notification.rb
+++ backend/app/models/random_task_slack_notification.rb
@@ -1,18 +1,15 @@
 class RandomTaskSlackNotification
     NOT_SET_ENV_KEY_MESSAGE = "SlackのWebhookURLが環境変数に設定されていません。.envをご確認下さい"
-    EXIST_TASK_MESSAGE      = "タスクが存在しません。画面からタスクを追加して下さい"
+    EXIST_TASK_MESSAGE      = "[Warn]タスクが存在しません。画面からタスクを追加して下さい"
 
     # タスク全件からランダムに１件Slackへ通知する
     def exec
-        puts Rails.logger.info "入った！！"
         begin
             unless ENV.key?('SLACK_WEB_HOOK_URL')
                 raise StandardError, NOT_SET_ENV_KEY_MESSAGE
             end
 
             task = random_task
-            puts task
-
             if task.nil?
                 raise StandardError, EXIST_TASK_MESSAGE
             end
@@ -33,8 +30,6 @@ class RandomTaskSlackNotification
     # タスクからランダムに１件取得
     # @return [Task]
     def random_task
-        puts Rails.logger.info Task.first.id
-        puts Rails.logger.info Task.last.id
-        return Task.where( 'id >= ?', rand(Task.first.id..Task.last.id) ).first
+        return Task.offset( rand(Task.count) ).first
     end
 end
