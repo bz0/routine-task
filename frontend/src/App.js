5678f924bf7e5a0e4708f2636b24e1731d340dda
bz0 <bzz0217@yahoo.co.jp>
2022-03-30 14:00:35 +0900

    [front]一覧の取得 WIP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   ChakraProvider,
   Flex,
@@ -8,11 +8,31 @@ import {
   FormLabel,
   Box,List,
   Input,Button,ListItem,
-  Spacer,Stack
+  Spacer
 } from '@chakra-ui/react';
 import { BiTask } from 'react-icons/bi';
 
 function App() {
+  const [tasks, setTask] = useState();
+  useEffect(() => {
+      create();
+  });
+
+  const create = async () => {
+    // countに応じてアクセスするAPIを変えなければならない
+    const url = 'http://' + process.env.REACT_APP_BACKEND_DOMAIN + '/tasks';
+    console.log("url:",url)
+
+    const response = await fetch(url,{
+      headers: {
+        Authorization: "Bearer " + process.env.REACT_APP_API_TOKEN
+      }
+    });
+    const body = await response.json();
+    console.log("body:", body)
+    setTask(body.data); // stateに反映する
+  };
+
   return (
     <ChakraProvider theme={theme}>
         <Flex justifyContent="flex-start" px={8} py={3} backgroundColor="#000000" color="whiteAlpha.800">
@@ -31,21 +51,23 @@ function App() {
           </FormControl>
 
           <List mt={10}>
-            <ListItem border="1px solid #eee" px={8} py={3} display="block">
-              <Flex>
-                <Flex w='60%'>
-                  <Box my='auto'><BiTask /></Box>
-                  <Box ml='5' my='auto'>掃除する</Box>  
+            {tasks && tasks.map((task, key) =>
+              <ListItem border="1px solid #eee" px={8} py={3} display="block" key={key}>
+                <Flex>
+                  <Flex w='60%'>
+                    <Box my='auto'><BiTask /></Box>
+                    <Box ml='5' my='auto'>{task.name}</Box>  
+                  </Flex>
+                  <Spacer />
+                  <Box>
+                    <Button size='xs'>編集</Button>
+                  </Box>
+                  <Box>
+                    <Button size='xs' ml='5' colorScheme='pink'>削除</Button>
+                  </Box>
                 </Flex>
-                <Spacer />
-                <Box>
-                  <Button size='xs'>編集</Button>
-                </Box>
-                <Box>
-                  <Button size='xs' ml='5' colorScheme='pink'>削除</Button>
-                </Box>
-              </Flex>
-            </ListItem>
+              </ListItem>
+            )}
           </List>
         </Box>
     </ChakraProvider>
