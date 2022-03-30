fd6ad29a5ccb2c49cc3388b905720250270abdb8
bz0 <bzz0217@yahoo.co.jp>
2022-03-30 12:28:50 +0900

    [front]ホーム画面のレイアウト実装 WIP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,53 @@
 import React from 'react';
 import {
   ChakraProvider,
-  Box,
-  Menu,
-  MenuItem,MenuButton,MenuList,
-  Grid,
+  Flex,
   theme,
+  Heading,
+  FormControl,
+  FormLabel,
+  Box,List,
+  Input,Button,ListItem,
+  Spacer,Stack
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { BiTask } from 'react-icons/bi';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <Menu>
-            aaaa
-            <MenuList>
-              <MenuItem>Download</MenuItem>
-              <MenuItem>Create a Copy</MenuItem>
-              <MenuItem>Mark as Draft</MenuItem>
-              <MenuItem>Delete</MenuItem>
-              <MenuItem>Attend a Workshop</MenuItem>
-            </MenuList>
-          </Menu>
-        </Grid>
-      </Box>
+        <Flex justifyContent="flex-start" px={8} py={3} backgroundColor="#000000" color="whiteAlpha.800">
+          <Heading size="sm">習慣化タスクリマインダー</Heading>
+        </Flex>
+
+        <Box width="50%" m="auto" minHeight="100%">
+          <FormControl mt={5}>
+            <FormLabel fontWeight="bold">タスクを追加する</FormLabel>
+            <Flex>
+              <Input />
+              <Button variant="solid" size="md" ml={5}>
+                追加
+              </Button>
+            </Flex>
+          </FormControl>
+
+          <List mt={10}>
+            <ListItem border="1px solid #eee" px={8} py={3} display="block">
+              <Flex>
+                <Flex w='60%'>
+                  <Box my='auto'><BiTask /></Box>
+                  <Box ml='5' my='auto'>掃除する</Box>  
+                </Flex>
+                <Spacer />
+                <Box>
+                  <Button size='xs'>編集</Button>
+                </Box>
+                <Box>
+                  <Button size='xs' ml='5' colorScheme='pink'>削除</Button>
+                </Box>
+              </Flex>
+            </ListItem>
+          </List>
+        </Box>
     </ChakraProvider>
   );
 }
