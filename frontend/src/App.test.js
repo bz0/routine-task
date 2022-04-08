c8624d3b058efe840ce73efad1e1057ce18a5e1d
bz0 <bzz0217@yahoo.co.jp>
2022-04-08 11:51:25 +0900

    jest:テスト WIP

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,10 +1,46 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
 import { render } from './test-utils';
-import App from './App';
+import Home from './pages/Home';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn chakra/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const tasksResult = {
+  message: "ok",
+  count: 3,
+  data:[
+    {
+      name: "散歩する",
+      created_at: "2022-04-08 00:00:00",
+      updated_at: "2022-04-08 00:00:00",
+      deleted_at: null
+    },
+    {
+      name: "掃除する",
+      created_at: "2022-04-08 00:00:00",
+      updated_at: "2022-04-08 00:00:00",
+      deleted_at: null
+    },
+    {
+      name: "体操する",
+      created_at: "2022-04-08 00:00:00",
+      updated_at: "2022-04-08 00:00:00",
+      deleted_at: null
+    }
+  ]
+}
+
+test("3件表示", async () => {
+  const tasksMock = () =>
+    new Promise((resolve) => {
+      resolve({
+        ok: true,
+        status: 200,
+        json: async () => (tasksResult)
+      })
+    })
+  
+  global.fetch = jest.fn().mockImplementation(tasksMock)
+  const tasks = render(<Home />)
+
+  //出力されるカード数と件数をチェック
+  expect(await tasks.getByTestId('count').textContent).toBe("3")
+  expect(await tasks.getByTestId('tasks').childElementCount).toBe(3)
+})
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -74,13 +74,13 @@ export const Home = () => {
             </Box>
             <Box my='auto' display='flex' ml='5' mr='3'>
               <Text my="auto">全</Text>
-              <Text mx="2" my="auto" fontSize='2xl' fontWeight='bold' color='pink.500'>{tasks.length}</Text>
+              <Text mx="2" my="auto" fontSize='2xl' fontWeight='bold' color='pink.500' data-testid="count">{tasks.length}</Text>
               <Text my="auto">件</Text>
             </Box>
           </Flex>
           <List mt={2}>
             {tasks.length > 0 ? tasks.map((task, index) =>
-              <TaskRow task={task} getList={getList} index={index} key={task.id} />
+              <TaskRow task={task} getList={getList} index={index} key={task.id} data-testid="tasks" />
             ) :   <Alert status='error' mt='10' variant='left-accent'>
                     <AlertIcon /> タスクがありません。タスクを追加して下さい
                   </Alert>}
